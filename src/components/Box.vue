0c9073ec1e1e146bd9fee83870b7c6fde2e6465d
vivifds <vivianefds@gmail.com>
2025-12-19 10:23:19 -0300

    primeira alteracao

---
+++ src/components/Box.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Box',
+  data () {
+    return {
+      estilos: {
+        backgroundColor: '#FAF0CA'
+      }
+    }
+  }
+})
+</script>
+
